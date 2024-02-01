c04c0f3671da40fbeaf610e686a583509fd7e959
nielsencampos-snowflakecourses <158470757+nielsencampos-snowflakecourses@users.noreply.github.com>
2024-02-01 09:17:28 -0300

    Update streamlit_app.py
    added fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit, pandas
 
 streamlit.title('My Parents New Healthy Dines')
 
@@ -6,3 +6,6 @@ streamlit.header('🍞 Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
