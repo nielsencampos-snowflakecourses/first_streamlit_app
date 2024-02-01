c1b49dbe103a28592b5a063b3b9095cca6c6a965
nielsencampos-snowflakecourses <158470757+nielsencampos-snowflakecourses@users.noreply.github.com>
2024-02-01 09:32:59 -0300

    Update streamlit_app.py
    correcting presseting of indexes

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index("Fruit", ["Avocado", "Strawberries"])
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list. index))
+my_fruit_list = my_fruit_list.set_index("Fruit")
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list. index), ["Avocado", "Strawberries"])
 
 streamlit.dataframe(my_fruit_list)
