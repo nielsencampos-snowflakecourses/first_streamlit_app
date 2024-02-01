2bc3b4d4650345ff94ae038bab040aab6b13dd7e
nielsencampos-snowflakecourses <158470757+nielsencampos-snowflakecourses@users.noreply.github.com>
2024-02-01 11:32:09 -0300

    Update streamlit_app.py
    requests #4

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,13 @@ streamlit.multiselect("Pick some fruits: ", list(my_fruit_list. index), ["Avocad
 
 streamlit.dataframe(my_fruit_list)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 streamlit.header('Fruityvice Fruit Advice!')
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
